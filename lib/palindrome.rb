66d9123e2cda92c2d3e6785e48589d423cb9b70c
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-23 23:24:13 -0700

    PASS test for spec 5: Scans a .txt file for palindromes, but does not work with punctuation yet

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -4,7 +4,11 @@ class PalindromeChecker
     @input = input.to_s
   end
 
-  def palindrome?
+  def palindrome
+    if @input.include?(".txt")
+      palindrome_array = File.read(@input).split(" ").select{|a| a.downcase == a.downcase.reverse}
+      return "Total Palindromes = #{palindrome_array.length}: #{palindrome_array.join(", ")}"
+    end
     return @input.downcase == @input.downcase.reverse
   end
 
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -1,25 +1,30 @@
 require ('rspec')
 require ('palindrome')
 
-describe('PalindromeChecker#palindrome?') do
+describe('PalindromeChecker#palindrome') do
   it('checks if a word is a palindrome') do
     word = PalindromeChecker.new('mom')
-    expect(word.palindrome?).to(eq(true))
+    expect(word.palindrome).to(eq(true))
+  end
+
+  it('checks if a word is a palindrome') do
+    word = PalindromeChecker.new('cat')
+    expect(word.palindrome).to(eq(false))
   end
 
   it('checks if a word-regardless of capitalization-is a palindrome') do
     word = PalindromeChecker.new('Mom')
-    expect(word.palindrome?).to(eq(true))
+    expect(word.palindrome).to(eq(true))
   end
 
   it('checks if an integer is a palindrome') do
     int = PalindromeChecker.new(2662)
-    expect(int.palindrome?).to(eq(true))
+    expect(int.palindrome).to(eq(true))
   end
 
   it('scans a document for palindromes') do
-    doc = PalindromeChecker.new('palindromes.txt')
-    expect(doc.palindrome?).to(eq("Total Palindromes = 3: Mom, eye, racecar"))
+    doc = PalindromeChecker.new("palindromes.txt")
+    expect(doc.palindrome).to(eq("Total Palindromes = 3: Mom, eye, racecar"))
   end
 
 end
